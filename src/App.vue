94c7b757aea6344723f2e860e3467405da1e54a3
gabrisca <gabrielescarparo1985@gmail.com>
2021-05-26 12:47:42 +0200

    secondo commit

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
 
@@ -13,7 +14,31 @@ export default {
   name: 'App',
   components: { 
     Header, 
-    Main },
+    Main 
+  },
+  data(){
+    return{
+      apiURL:"https://api.themoviedb.org/3/search/movie",
+      apiKey: "8aba2f0fc3e09fb1de7523aaaf3513bc",
+      // ricordati il v-model stringa vuota
+      query: "ritorno al futuro"
+    }
+  },
+  created() {
+    axios.get(this.apiUrl, {
+      params: {
+        api_Key: this.apiKey,
+        query: this.query,
+        language: 'it-IT'
+      }
+    })
+    .then((resp)=>{
+      console.log(resp.data.results);
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  },
 }
 
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,18 @@
 <template>
-  <header>
-    header
+  <header class="d-flex justify-content-between align-items-center ps-5 pe-5">
+    <div>logo</div>
+    <div>
+      <form class="d-flex mt-3 mb-3">
+    <div>
+     <input class="form-control" type="text" placeholder="Find your movie"> 
+    </div>
+    <div class="ms-3">
+      <button class="btn btn-primary mc_btn"> 
+        Search
+      </button>
+    </div>
+  </form>
+    </div>
   </header>
 </template>
 
@@ -11,5 +23,17 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-
+/* // importo le variabili */
+@import '../assets/style/vars.scss';
+  header{
+    background: $color-header;
+    height: 65px;
+    .mc_btn {
+      background-color: $color-btn;
+      border-color: transparent;
+      &:hover{
+        filter: brightness(1.2);
+      }
+    }
+  }
 </style>
